a9cb0e342c71962bd42cd21e3777d6ffba9c5998
amv1017 <amv1017@mail.ru>
2021-05-09 19:05:12 +0300

    chapter 10

--- go-admin/controllers/authController.go
+++ go-admin/controllers/authController.go
@@ -6,10 +6,24 @@ import (
 )
 
 func Register(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "password do not match",
+		})
+	}
+
 	user := models.User{
-		FirstName: "John",
-		LastName: "White",
-		Email: "johnwhite@email.com",
+		FirstName: data["first_name"],
+		LastName: data["last_name"],
+		Email: data["email"],
+		Password: data["password"],
 	}
 
 	return c.JSON(user)
