32d8cf5e1802be2d40376d2e6805ef43684feb36
amv1017 <amv1017@mail.ru>
2021-05-09 22:53:16 +0300

    chapter 19

--- go-admin/routes/routes.go
+++ go-admin/routes/routes.go
@@ -7,10 +7,17 @@ import (
 )
 
 func Setup(app *fiber.App) {
+
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login",    controllers.Login)
+
 	app.Use(middlewares.IsAuthenticated)
+
 	app.Get( "/api/user",     controllers.User)
 	app.Post("/api/logout",   controllers.Logout)
+
+	app.Get( "/api/users",    controllers.AllUsers)
+	app.Post( "/api/users",   controllers.CreateUser)
+
 }
 
