1fc7ffb1c1f3a89be5c6e89602cd8eb069e47021
amv1017 <amv1017@mail.ru>
2021-05-15 15:44:23 +0300

    chapter 32

--- go-admin/models/order.go
+++ go-admin/models/order.go
@@ -6,9 +6,11 @@ import (
 
 type Order struct {
 	Id			uint		`json:"id"`
-	FirstName	string		`json:"first_name"`
-	LastName	string		`json:"last_name"`
+	FirstName	string		`json:"-"`
+	LastName	string		`json:"-"`
+	Name		string		`json:"name" gorm:"-"`
 	Email		string		`json:"email"`
+	Total		float32		`json:"total" gorm:"-"`
 	UpdateAt	string		`json:"updated_at"`
 	CreatedAt	string		`json:"created_at"`
 	OrderItems	[]OrderItem	`json:"order_items" gorm:"foreignKey:OrderId"`
@@ -31,6 +33,17 @@ func (order *Order) Count(db *gorm.DB) int64 {
 func (order *Order) Take(db *gorm.DB, limit int, offset int) interface{} {
 	var orders []Order
 	db.Preload("OrderItems").Offset(offset).Limit(limit).Find(&orders) // loading foreign key
+	
+	
+	for i, _ := range orders {
+		orders[i].Name = orders[i].FirstName + " " + orders[i].LastName
+		var total float32 = 0
+		for _, orderItem := range orders[i].OrderItems {
+			total += orderItem.Price * float32(orderItem.Quantity)
+		}
+		orders[i].Total = total
+	}
+
 	return orders
 }
 
