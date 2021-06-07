bc56a8c98fe2b1deff33f4288087616a59279560
amv1017 <amv1017@mail.ru>
2021-06-07 20:07:56 +0300

    chapter 44

--- react-admin/src/pages/Login.tsx
+++ react-admin/src/pages/Login.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+
+    const [count, setCount] = useState(0)
+
     return (
-        <div>
-            Login
+        <div className="container">
+            <h1>Count {count}</h1>
+            <input type="number" onChange={e => setCount(parseInt(e.target.value))}></input>
         </div>
     )
 }
