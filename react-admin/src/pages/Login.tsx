82ee582b867ab4bb9b3098cc61a791d79b3a7ad4
amv1017 <amv1017@mail.ru>
2021-06-14 23:25:45 +0300

    chapter 45

--- react-admin/src/pages/Login.tsx
+++ react-admin/src/pages/Login.tsx
@@ -1,13 +1,44 @@
-import React, { useState } from 'react'
+import React, { SyntheticEvent, useState } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 export default function Login() {
 
-    const [count, setCount] = useState(0)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false)
+
+    const submit = async (e: SyntheticEvent) => {
+
+        e.preventDefault()
+        const { data } = await axios.post('http://localhost:8080/api/login', {
+            email,
+            password
+        }, { withCredentials: true })
+
+        setRedirect(true)
+
+    }
+
+    if (redirect) {
+        return <Redirect to={'/'} />
+    }
 
     return (
-        <div className="container">
-            <h1>Count {count}</h1>
-            <input type="number" onChange={e => setCount(parseInt(e.target.value))}></input>
-        </div>
+        <main className="form-signin">
+            <form onSubmit={submit}>
+                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+                <input type="email" className="form-control" placeholder="user@email.com" required
+                    onChange={e => setEmail(e.target.value)}
+                />
+
+                <input type="password" className="form-control" placeholder="password" required
+                    onChange={e => setPassword(e.target.value)}
+                />
+
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+            </form>
+    </main>
     )
 }
