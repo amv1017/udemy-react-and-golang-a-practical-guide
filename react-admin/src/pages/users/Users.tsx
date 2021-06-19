c5febd6c5ac911e9d215f892c9d3537825273946
amv1017 <amv1017@mail.ru>
2021-06-20 01:32:09 +0300

    chapter 52

--- react-admin/src/pages/users/Users.tsx
+++ react-admin/src/pages/users/Users.tsx
@@ -6,15 +6,30 @@ import { User } from '../../models/user'
 const Users = () => {
 
 	const [users, setUsers] = useState([])
+	const [page, setPage] = useState(1)
+	const [lastPage, setLastPage] = useState(0)
 
 	useEffect(() => {
 		(
 			async () => {
-				const {data} = await axios.get('users')
+				const {data} = await axios.get(`users?page=${page}`)
 				setUsers(data.data)
+				setLastPage(data.meta.last_page)
 			}
 		)()
-	}, [])
+	}, [page])
+
+	const next = () => {
+		if (page <= lastPage) {
+			setPage(page+1)
+		}
+	}
+
+	const previous = () => {
+		if (page > 1) {
+			setPage(page-1)
+		}
+	}
 
 	return (
 		<Wrapper>
@@ -44,6 +59,18 @@ const Users = () => {
 				</tbody>
 			</table>
 			</div>
+
+			<nav>
+				<ul className="pagination">
+					<li className="page-item">
+						<a href="#" className="page-link" onClick={previous}>Previous</a>
+					</li>
+					<li className="page-item">
+						<a href="#" className="page-link" onClick={next}>Next</a>
+					</li>					
+				</ul>
+			</nav>
+
 		</Wrapper>
 	)
 }
