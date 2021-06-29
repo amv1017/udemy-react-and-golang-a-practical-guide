a9c2f594b39e5bce8f9c4901314c4c58731d181b
amv1017 <amv1017@mail.ru>
2021-06-30 01:21:56 +0300

    chapter 54

--- react-admin/src/pages/users/Users.tsx
+++ react-admin/src/pages/users/Users.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Wrapper from '../../components/Wrapper'
 import axios from 'axios'
 import { User } from '../../models/user'
+import { Link } from 'react-router-dom'
 
 const Users = () => {
 
@@ -31,8 +32,19 @@ const Users = () => {
 		}
 	}
 
+	const del = async (id: number) => {
+		if (window.confirm('Are you sure you want to delete this record?')) {
+			await axios.delete(`users/${id}`)
+			setUsers(users.filter((u: User) => u.id !== id))
+		}
+	}
+
 	return (
 		<Wrapper>
+			<div className="pt-3 pb-2 mb-3 border-bottom">
+				<Link to="/users/create" className="btn btn-sm btn-outline-secondary">Add</Link>
+			</div>
+
 			<div className="table-responsive">
 			<table className="table table-striped table-sm">
 				<thead>
@@ -52,7 +64,14 @@ const Users = () => {
 								<td>{user.first_name} {user.last_name}</td>
 								<td>{user.email}</td>
 								<td>{user.role.name}</td>
-								<td></td>
+								<td>
+									<div className="btn-group mr-2">
+										<a href="#" 
+											className="btn btn-sm btn-outline-secondary"
+											onClick={() => del(user.id)}
+										>Delete</a>
+									</div>
+								</td>
 							</tr>
 						)
 					})}
